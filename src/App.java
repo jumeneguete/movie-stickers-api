be90a3d07a06e0d792d7f2f4493ee8e9f82dcd1d
Juliana Meneguete <jumeneguete@gmail.com>
2022-07-18 21:35:04 -0300

    feat: http connection

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // conexão HTTP
+        String url = "https://alura-imdb-api.herokuapp.com/movies";
+        URI uri = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+    }
+}
