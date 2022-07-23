51c0c3f1195fd631a7abfc116b953937d2279005
Juliana Meneguete <jumeneguete@gmail.com>
2022-07-23 19:25:15 -0300

    feat: update App to call StickerGenerator

--- src/App.java
+++ src/App.java
@@ -5,12 +5,14 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+import java.io.InputStream;
+import java.net.URL;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // conexão HTTP
-        String url = "https://alura-imdb-api.herokuapp.com/movies";
-        URI uri = URI.create(url);
+        String ApiUrl = "https://alura-imdb-api.herokuapp.com/movies";
+        URI uri = URI.create(ApiUrl);
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
@@ -21,12 +23,18 @@ public class App {
         List<Map<String, String>> movieList = parser.parse(body);
 
         // exibir dados
+        StickerGenerator generateSticker = new StickerGenerator();
+  
+        for (int i = 0; i < 40; i++) {
+            // InputStream lendo de uma URL
+            String imageUrl = movieList.get(i).get("image");
+            InputStream inputStream = new URL(imageUrl).openStream(); //openStream, tipo "abra essa conexão e me retorne os bytes que estão lá"
+            String title = movieList.get(i).get("title");
+            Double imDbRating = Double.parseDouble(movieList.get(i).get("imDbRating"));
 
-        for (int i = 0; i < movieList.size(); i++) {
-            System.out.println(i + 1 + ": " + movieList.get(i).get("title"));
-            System.out.println("Nota: " + movieList.get(i).get("imDbRating"));
-            System.out.println("Link da imagem: " + movieList.get(i).get("image"));
+            generateSticker.generate(inputStream, title, imDbRating);
 
+            System.out.println(i + 1 + ": " + title);
             System.out.println();
         }
 
